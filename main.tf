c64bfa87c98085979601a83850be3cc6b8d0563b
Chichi126 <chinyere.nwigwe126@gmail.com>
2024-08-12 13:00:22 +0100

    added workings

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_resource_group" "firstrg" {
+  name     = "chidec"
+  location = "West US"
+}
+
+resource "azurerm_storage_account" "firstchistorage" {
+  name                     = "chistoragedec"
+  resource_group_name      = azurerm_resource_group.firstrg.name
+  location                 = azurerm_resource_group.firstrg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  enable_https_traffic_only = true
+  account_kind              = "StorageV2"
+  is_hns_enabled            = true  # Enable hierarchical namespace (ADLS Gen2)
+
+
+  tags = {
+    environment = "staging"
+  }
+}
