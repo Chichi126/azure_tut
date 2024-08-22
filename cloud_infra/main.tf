310fd77d73fef8ce4a25fd93510c30c60d50b0ec
Chichi126 <chinyere.nwigwe126@gmail.com>
2024-08-22 19:18:55 +0100

    edit codes

--- cloud_infra/main.tf
+++ cloud_infra/main.tf
@@ -4,14 +4,14 @@ resource "azurerm_resource_group" "firstrg" {
 }
 
 resource "azurerm_storage_account" "firstchistorage" {
-  name                     = "chistoragedec"
-  resource_group_name      = azurerm_resource_group.firstrg.name
-  location                 = azurerm_resource_group.firstrg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+  name                      = "chistoragedec"
+  resource_group_name       = azurerm_resource_group.firstrg.name
+  location                  = azurerm_resource_group.firstrg.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
   enable_https_traffic_only = true
   account_kind              = "StorageV2"
-  is_hns_enabled            = true  # Enable hierarchical namespace (ADLS Gen2)
+  is_hns_enabled            = true # Enable hierarchical namespace (ADLS Gen2)
 
 
   tags = {
